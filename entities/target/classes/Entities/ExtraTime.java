45db39a978935bd9c6db167ed5b542bf29d54294
Omer <omerartzi5@Gmail.com>
2023-05-23 19:11:17 +0300

    added connection to data base and hashing

---
+++ entities/target/classes/Entities/ExtraTime.java
@@ -0,0 +1,10 @@
+package Entities;
+
+import java.util.List;
+
+public class ExtraTime {
+    private List<Principle> principles;
+    public List<Principle> getPrincipals() {
+        return this.principles;
+    }
+}
