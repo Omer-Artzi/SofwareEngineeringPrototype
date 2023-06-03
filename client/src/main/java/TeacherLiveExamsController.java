8b3d2716f16e78f6d369bbdccb35c13754a8a2f5
Liad1403 <115228691+Liad1403@users.noreply.github.com>
2023-06-03 19:46:09 +0300

    Changes in LiveExamsController

--- client/src/main/java/TeacherLiveExamsController.java
+++ client/src/main/java/TeacherLiveExamsController.java
@@ -1,5 +1,4 @@
-import Entities.ClassExam;
-import Entities.Message;
+import Entities.*;
 import Events.LiveExamsEvent;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
@@ -13,6 +12,7 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class TeacherLiveExamsController {
@@ -38,27 +38,70 @@ public class TeacherLiveExamsController {
     @FXML
     private TableColumn<ClassExam, String> SubjectColumn;
 
+    public List<ClassExam> SelectedExams(List<ClassExam> examList) throws IOException {
+        Date CurrentDate=new Date();
+        List<ClassExam>LiveExam=new ArrayList<>();
+
+        /* First check : if the ClassExam's start and end date is between the current date*/
+        for(ClassExam item: examList)
+        {
+            if (item.getStartDate().after(CurrentDate)||item.getStartDate().equals(CurrentDate))
+                if (item.getFinalSubmissionDate().before(CurrentDate))
+                    LiveExam.add(item);
+        }
+        /* Second check : show only the exams of the current student/ teacher, or show all in case the user is principle*/
+        Person person=SimpleClient.getClient().getUser();
+
+        /* case 1: the user is principle*/
+        if(person instanceof Principle)
+            return LiveExam;
+
+        /*case 2: the user is teacher */
+        else if(person instanceof Teacher){
+            RequestExtraTimeBT.setVisible(true);
+            for(ClassExam item: LiveExam)
+                if(!item.getTeacher().equals(person))
+                    LiveExam.remove(item);
+        }
+
+        /* case 3: the user is Student */
+        else{
+            boolean b=false;
+            for(ClassExam item: LiveExam){
+                for(Student item1: item.getStudents())
+                {
+                    if (item1.equals(person))
+                       b=true;
+                }
+                if(b==false)
+                    LiveExam.remove(item);
+            }
+        }
+        return LiveExam;
+    }
+
+
     @Subscribe
-    public void update(LiveExamsEvent event) {
+    public void update(LiveExamsEvent event) throws IOException {
         System.out.println("In LiveExamsController");
         examList=event.getLiveExams();
-        //data = FXCollections.observableArrayList(
-       //         event.getLiveExams()
-       // );
+        List<ClassExam>LiveExam=SelectedExams(event.getLiveExams());
         if (!examList.isEmpty())
         {
             for (ClassExam item:examList)
             {
                 System.out.println("hi");
+                //if(item.)
             }
         }
-        data.addAll(examList);
+        data.addAll(LiveExam);
     }
 
 
     @FXML
    void initialize() throws IOException {     // TODO:fill this function after merging with lior
         EventBus.getDefault().register(this);
+        RequestExtraTimeBT.setVisible(false);
         //data=new TableView<>();
         data = ExamsTable.getItems();
         SubjectColumn.setCellValueFactory(new PropertyValueFactory<>("subject"));
